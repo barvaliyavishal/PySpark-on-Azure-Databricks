ec145bd506e563fcba26df8ee6ae3b2227dbb787
vishalbarvaliya112@gmail.com <vishalbarvaliya112@gmail.com>
2023-01-13 00:47:46 +0000

    read file csv and json

--- Reading and writing data from files using Spark's Dataframe/reading data from text files into dataframe.py
+++ Reading and writing data from files using Spark's Dataframe/reading data from text files into dataframe.py
@@ -1,9 +1,45 @@
 # Databricks notebook source
-users = spark.read.csv("dbfs:/Workspace/Repos/c0851464@mylambton.ca/PySpark-on-Azure-Databricks/Reading and writing data from files using Spark's Dataframe/users.csv")
+users = spark.read.json('/public/retail_db/user.json')
+
+# COMMAND ----------
+
+users.show()
 
 # COMMAND ----------
 
 # MAGIC %fs ls "/public/retail_db"
+
+# COMMAND ----------
+
+users.write.parquet("/public/retail_db/write_parquet/users.parquet")
+
+# COMMAND ----------
+
+# MAGIC %fs ls "/public/retail_db/write_parquet"
+
+# COMMAND ----------
+
+u = spark.read.parquet("/public/retail_db/write_parquet/users.parquet")
+
+# COMMAND ----------
+
+u.show()
+
+# COMMAND ----------
+
+users.show()
+
+# COMMAND ----------
+
+users.write.csv("/public/retail_db/write/csv/users.csv",mode="overwrite",sep='|')
+
+# COMMAND ----------
+
+# MAGIC %fs ls "/public/retail_db/write/csv/"
+
+# COMMAND ----------
+
+spark.read.csv("/public/retail_db/write/csv/users.csv",sep='|').show()
 
 # COMMAND ----------
 
