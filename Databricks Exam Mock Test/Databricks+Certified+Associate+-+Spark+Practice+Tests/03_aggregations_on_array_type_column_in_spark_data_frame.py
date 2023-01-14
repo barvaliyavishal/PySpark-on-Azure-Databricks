08329316b78a958524ea360a0bf79470484feb05
vishalbarvaliya112@gmail.com <vishalbarvaliya112@gmail.com>
2023-01-14 23:33:36 +0000

    Databricks Exam Material

---
+++ Databricks Exam Mock Test/Databricks+Certified+Associate+-+Spark+Practice+Tests/03_aggregations_on_array_type_column_in_spark_data_frame.py
@@ -0,0 +1,73 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## Aggregations on Array Type Column in Spark Data Frame
+# MAGIC 
+# MAGIC Develop a function to get number of times the color is repeated in arrays in the Data Frame. The logic should check whether the color exists or not.
+# MAGIC * The input data is in the form of Spark Data Frame. Each column in the Data Frame is of type Array.
+# MAGIC * Each Array contain multiple colors.
+# MAGIC * The function should take 2 arguments. The first argument is Spark Data Frame which contain colors. The second argument is the color which you want to check.
+# MAGIC * If the color that is passed as second argument, exists in any of the tuples, then the function should return number of times it is present across all the rows of Arrays.
+# MAGIC * The characters in the second argument might be passed in any case. However, we need to perform case insensitive search.
+
+# COMMAND ----------
+
+colors = [
+    (('Red', 'White', 'Yellow'),),
+    (('YELLOW', 'Pink', 'red'),),
+    (('blue', 'Yellow', 'Lime'),)
+]
+
+colors_df = spark.createDataFrame(colors, schema='colors ARRAY<STRING>')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Step 1: Preview the data
+# MAGIC * Let us first preview the data.
+
+# COMMAND ----------
+
+colors_df
+
+# COMMAND ----------
+
+colors_df.count()
+
+# COMMAND ----------
+
+colors_df.show()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Step 2: Provide the solution
+# MAGIC Now come up with the solution by developing the required logic. Once the function is developed, go to the next step to take care of the validation.
+
+# COMMAND ----------
+
+def color_count(colors_df, color):
+    # your code should go here
+    return cnt
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Step 3: Validate the function
+# MAGIC 
+# MAGIC * Here is the expected output.
+
+# COMMAND ----------
+
+color_count(colors_df, 'Red') # 2
+
+# COMMAND ----------
+
+color_count(colors_df, 'red') # 2
+
+# COMMAND ----------
+
+color_count(colors_df, 'Black') # 0
+
+# COMMAND ----------
+
+color_count(colors_df, 'YELLOW') # 3
---
+++ Databricks Exam Mock Test/Databricks+Certified+Associate+-+Spark+Practice+Tests/10_cleanup_keys_for_mobile_details.py
@@ -0,0 +1,102 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## Cleanup Spark Data Frame Columns for mobile details
+# MAGIC 
+# MAGIC Develop logic to remove **mobile** from the column names in the Data Frame.
+
+# COMMAND ----------
+
+mobile_details = [{'mobile_company': 'Apple',
+  'mobile_model': 'Iphone 12',
+  'mobile_ram': 4,
+  'mobile_storage': 256,
+  'mobile_os': 'iOS'},
+ {'mobile_company': 'Xiaomi',
+  'mobile_model': 'Redmi 9',
+  'mobile_ram': 4,
+  'mobile_storage': 64,
+  'mobile_os': 'android'},
+ {'mobile_company': 'One plus',
+  'mobile_model': 'One plus 9R',
+  'mobile_ram': 6,
+  'mobile_storage': 128,
+  'mobile_os': 'android'},
+ {'mobile_company': 'Samsung',
+  'mobile_model': 'Galaxy M32',
+  'mobile_ram': 4,
+  'mobile_storage': 64,
+  'mobile_os': 'android'},
+ {'mobile_company': 'Xiaomi',
+  'mobile_model': 'Redmi note 10 pro',
+  'mobile_ram': 8,
+  'mobile_storage': 128,
+  'mobile_os': 'android'},
+ {'mobile_company': 'One plus',
+  'mobile_model': 'One plus Nord',
+  'mobile_ram': 4,
+  'mobile_storage': 128,
+  'mobile_os': 'android'},
+ {'mobile_company': 'Apple',
+  'mobile_model': 'Iphone X',
+  'mobile_ram': 4,
+  'mobile_storage': 256,
+  'mobile_os': 'android'},
+ {'mobile_company': 'Oppo',
+  'mobile_model': 'Oppo A31',
+  'mobile_ram': 4,
+  'mobile_storage': 64,
+  'mobile_os': 'iOS'}]
+
+# COMMAND ----------
+
+mobile_details_df = spark.createDataFrame(mobile_details)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Step 1: Preview the data
+# MAGIC * Let us first preview the data.
+
+# COMMAND ----------
+
+display(mobile_details_df)
+
+# COMMAND ----------
+
+mobile_details_df.count()
+
+# COMMAND ----------
+
+mobile_details_df.columns
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Step 2: Provide the solution
+# MAGIC Now come up with the solution by developing the required logic. Once the logic is developed, go to the next step to take care of the validation.
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Step 3: Validate the function
+# MAGIC Run below cells for validation.
+
+# COMMAND ----------
+
+display(mobile_details_df_new)
+
+# COMMAND ----------
+
+mobile_details_df_new.columns # ['company', 'model', 'os', 'ram', 'storage']
+
+# COMMAND ----------
+
+
